0303ce887c282b72a2e9542dbc5bf67894cda5a8
NicoCastelnuovo <nico.castelnuovo@outlook.com>
2025-04-14 12:05:55 +0200

    Need to add electron OR pass the app to be tested

--- src/app/history/history-item/history-item.component.ts
+++ src/app/history/history-item/history-item.component.ts
@@ -1,11 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, signal } from '@angular/core';
 import { IOrder, Order } from '../../models';
 import { AmountType } from '../../models';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'history-item',
   standalone: true,
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './history-item.component.html',
   styleUrl: './history-item.component.scss'
 })
@@ -13,8 +16,13 @@ export class HistoryItemComponent implements OnInit {
   @Input() order: IOrder = new Order();
   @Output() deleteOrder = new EventEmitter();
 
+  total = signal(0);
+
   constructor() {}
 
   ngOnInit(): void {
+    this.total.set(this.order.units.reduce((accumulator, unit) => {
+      return accumulator + unit.subtotal;
+    }, 0))
   }
 }
